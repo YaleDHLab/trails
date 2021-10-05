50921c943d573ea06415dd3865733196a608720e
duhaime <douglas.duhaime@gmail.com>
2021-10-05 17:10:44 -0400

    add perlin noise + more colors

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://lab-apps/sketches/trails/ --profile yale-admin --acl public-read --exclude ".git/*" --exclude "assets/data/thumbs/*"
+aws s3 sync . s3://lab-apps/sketches/trails/ --profile yale-admin --acl public-read --exclude ".git/*"
