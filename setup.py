c9f31e1a0e4f4d0abf1dcc1b3155013de284fd36
duhaime <douglas.duhaime@gmail.com>
2021-10-15 16:22:08 -0400

    steps toward image pipeline

--- setup.py
+++ setup.py
@@ -30,8 +30,13 @@ setup(
   author_email='douglas.duhaime@gmail.com',
   license='MIT',
   install_requires=[
+    'colorthief==0.2.1',
     'glob2==0.7',
+    'imageio==2.9.0',
+    'numba==0.54.0',
     'numpy~=1.19.2',
+    'opencv-python==4.5.3.56',
+    'Pillow==8.3.2',
     'scikit-learn==0.24.2',
     'tensorflow==2.6.0',
     'tqdm==4.46.1',
